d6c3602525bafe3bd847909569fa2a2de17324ea
Abhirup Sadhukhan <abhirupsadhukhan@gmail.com>
2021-02-26 10:03:22 +0530

    base structure created

---
+++ DatabaseAdapter/Service/BaseDastabaseAdapter.cs
@@ -0,0 +1,34 @@
+﻿using DatabaseAdapter.Contract;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace DatabaseAdapter.Service
+{
+    public class BaseDastabaseAdapter : IDatabaseAdapter, IDisposable, IAsyncDisposable
+    {
+        protected DbConnection DbConnection { get; private set; }
+        public BaseDastabaseAdapter(DbConnection dbConnection)
+        {
+            DbConnection = dbConnection;
+        }
+        public void Dispose()
+        {
+            if (DbConnection.State != ConnectionState.Closed)
+                DbConnection.Close();
+            DbConnection = null;
+            GC.SuppressFinalize(this);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (DbConnection.State != ConnectionState.Closed)
+                await DbConnection.CloseAsync();
+            DbConnection = null;
+            GC.SuppressFinalize(this);
+        }
+    }
+}
